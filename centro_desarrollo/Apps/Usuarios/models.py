22b1642fd79ca90a42b79a3a8d5934fed02a961a
Bruno <brunopaz00@yahoo.com.mx>
2020-03-27 15:34:19 -0600

    Migratiosn tablas faltantes

--- centro_desarrollo/Apps/Usuarios/models.py
+++ centro_desarrollo/Apps/Usuarios/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
 
+
 def images_path():
     return os.path.join(settings.STATICFILES_DIRS,'img/disc_photos')
 
@@ -18,13 +19,33 @@ class Album(models.Model):
     nombre = models.CharField(max_length=50, null=False)
     duracion = models.DecimalField(max_digits=4, decimal_places=2,null=True) 
     fecha = models.DateField(auto_now=False ,auto_now_add=False)
-    foto = models.ImageField(upload_to=4,max_length=100)
+    foto = models.ImageField(upload_to=images_path,max_length=100)
 
 class PlayList(models.Model):
     is_public = models.BooleanField()
     nombre = models.CharField(max_length=30)
-    usuario = models.ForeignKey(User,on_delete=models.CASCADE)
+    usuario = models.ForeignKey('Usuario',on_delete=models.CASCADE)
 
 class PlayListCanciones(models.Model):
     playlist = models.ForeignKey('PlayList',on_delete=models.CASCADE)
     cancion = models.ForeignKey('Cancion',on_delete=models.CASCADE)
+
+class Disquera(models.Model):
+    nombre = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50)
+
+class UsuarioCanciones(models.Model):
+    id_usuario = models.ForeignKey('Usuario',on_delete=models.CASCADE)
+    id_cancion = models.ForeignKey('Cancion',on_delete=models.CASCADE)
+
+class Generos(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=50)
+
+class Usuario(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_premium = models.BooleanField()
+    fecha_nacimiento = models.DateField()
+    pais = models.CharField(max_length=100)
+    foto = models.ImageField(upload_to=images_path,max_length=100)
+    is_artist = models.BooleanField()
